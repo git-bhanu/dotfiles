4bf8a7e60f5a90b7349459a32a950d4adf5c64e8
Bhanu Singh <hey@bhanusingh.in>
2025-12-12 20:30:19 +0530

    updated color scheme to kanagawa

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,14 @@
 return {
   -- add gruvbox
-  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        -- see below
+      })
+      vim.cmd("colorscheme kanagawa")
+    end,
+  },
 }
